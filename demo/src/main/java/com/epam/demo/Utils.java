7a9fbc5294c9a23868659146d7fdc8d03ad53e29
saniazt <saniazt@gmail.com>
2022-10-30 21:25:04 +0200

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().mapToInt(Integer::parseInt).filter(x->x<0).findAny().isPresent();
+        return args.stream().allMatch(StringUtils:: isPositiveNumber);
     }
 }
